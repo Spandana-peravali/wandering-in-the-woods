846e7044e455083b31a0ae6a2da0fece15f909c9
Spandana-peravali <spandanaperavali@lewisu.edu>
2026-03-14 19:47:25 -0500

    Updated game code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 import pygame
 from mode_35 import run_35
+# from mode_k2 import run_k2
+# from mode_68 import run_68
 
 def main():
     pygame.init()
@@ -10,7 +11,9 @@ def main():
     
     while True:
         print("\n=== Wandering in the Woods ===")
+        # print("1. Grades K–2")
         print("2. Grades 3–5")
+        # print("3. Grades 6–8")
         print("4. Exit")
         print("Press ESC in game to return to menu")
         
@@ -32,31 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-from mode_35 import run_35
-# from mode_k2 import run_k2
-# from mode_68 import run_68
 
-
-def main():
-    while True:
-        print("\n=== Wandering in the Woods ===")
-        # print("1. Grades K–2")
-        print("2. Grades 3–5")
-        # print("3. Grades 6–8")
-        print("4. Exit")
-
-        choice = input("Select level (2-4): ")
-
-        if choice == "2":
-            run_35()
-        elif choice == "4":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice, try again!")
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 4aff5bc92a96347887be18b6ec9fdec42e5c94b1
